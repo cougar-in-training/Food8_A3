7b2a5d0cd243f0673889eb4e372c6cee871b19d1
Beatrice Johansson <annbeatrice.johansson@gmail.com>
2018-04-24 10:10:57 +0200

    changes in javascript

--- javascript/restauranter.js
+++ javascript/restauranter.js
@@ -34,7 +34,7 @@
 
                 grid_item_klon.querySelector("[data-h2]").textContent = restaurant.title.rendered;
                 grid_item_klon.querySelector("[data-img]").src = restaurant.acf.coverbillede.url;
-                grid_item_klon.querySelector("[data-restaurants-grid-item-id]").href = "http://127.0.0.1:59117/html/restauranter_single.html?id="+restaurant.id;
+                grid_item_klon.querySelector("[data-restaurants-grid-item-id]").href =  "./restauranter_single.html?id="+restaurant.id;
 
                 modtager.appendChild(grid_item_klon);
 
