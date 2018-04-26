d0011f2560307487e250b1eaaca8e1d3e163c1ed
Bruger <bruger@desktop-vn2dk54.kea.local>
2018-04-26 10:09:48 +0200

    slettede filer

--- javascript/footer.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load", hentFooter);
-
-async function hentFooter() {
-	let footerHentes = await fetch("footer.html");
-	let footer = await footerHentes.text();
-	document.querySelector("#footer").innerHTML = footer;
-}
