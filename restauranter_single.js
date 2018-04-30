7001b8c66486006e7083249cae1e3f4e13e9be27
Laise Bang Henriksen <31646151+LaiseBang@users.noreply.github.com>
2018-04-30 11:58:03 +0200

    ændre url fra id til restaurantnavn

--- restauranter_single.js
+++ restauranter_single.js
@@ -1,58 +1,57 @@
-
-    let url = new URL(window.location.href);
-	let parameter = new URLSearchParams(url.search);
-    let id = parameter.get("id");
+let url = new URL(window.location.href);
+let parameter = new URLSearchParams(url.search);
+let id = parameter.get("id");
 //    let id = "190";
 
-    let restaurant_single;
+let restaurant_single;
 
-    let modtager = document.querySelector(".modtager");
+let modtager = document.querySelector(".modtager");
 
-    let restaurant_single_template = document.querySelector("[data-single-restaurants]");
+let restaurant_single_template = document.querySelector("[data-single-restaurants]");
 
-    let klon = restaurant_single_template.cloneNode(true).content;
-
-
-    document.addEventListener("DOMContentLoaded", hentJson);
-
-    async function hentJson() {
-
-            let jsonObject = await fetch("http://josefinerasch.dk/kea/07-cms/food8/wordpress/wp-json/wp/v2/restauranter/" + id);
-            restaurant_single = await jsonObject.json();
-
-        if ( restaurant_single == null ){
-            document.querySelector(".modtager_single").innerHTML = "Ingen id eller sidan inte funnits"
-        }
+let klon = restaurant_single_template.cloneNode(true).content;
 
 
+document.addEventListener("DOMContentLoaded", hentJson);
 
-        visRestaurant_single();
-    }
+async function hentJson() {
 
-    function visRestaurant_single(){
+	let jsonObject = await fetch("http://josefinerasch.dk/kea/07-cms/food8/wordpress/wp-json/wp/v2/restauranter/" + id);
+	restaurant_single = await jsonObject.json();
 
-        klon.querySelector("[data-restaurant_splash]").src = restaurant_single.acf.coverbillede.url;
-        klon.querySelector("[data-restaurant_name]").innerHTML = restaurant_single.title.rendered;
-        klon.querySelector("[data-restaurant-overskrift]").innerHTML = restaurant_single.title.rendered;
-        klon.querySelector("[data-restaurant-manchet]").innerHTML = restaurant_single.acf.manchet;
-        klon.querySelector("[data-restaurant-description]").innerHTML = restaurant_single.acf.beskrivelse;
+	if (restaurant_single == null) {
+		document.querySelector(".modtager_single").innerHTML = "Ingen id eller sidan inte funnits"
+	}
 
-        klon.querySelector("[data-opening_hours_text]").innerHTML = "<h3>Åbningstider</h3>";
-        klon.querySelector("[data-contact_text]").innerHTML = "<h3>Kontakt</h3>";
-        klon.querySelector("[data-adress_text]").innerHTML = "<h3>Adresse</h3>";
-        klon.querySelector("[data-hygien_text]").innerHTML = "<h3>Kontrolrapport</h3>";
 
-        klon.querySelector("[data-opening-hours]").innerHTML = restaurant_single.acf.åbningstider;
-        klon.querySelector("[data-contact]").innerHTML = restaurant_single.acf.kontakt;
-        klon.querySelector("[data-adress]").innerHTML = restaurant_single.acf.adresse;
-        klon.querySelector("[data-hygien]").href = restaurant_single.acf.kontrolrapport;
-        document.querySelector("[data-page-title]").textContent = restaurant_single.title.rendered;
 
-        // console.log("Inserting image");
+	visRestaurant_single();
+}
 
-        let galleri_modtager = document.querySelector("[data-gallery-modtager]");
-        let acf = restaurant_single.acf
-        let billeder = [
+function visRestaurant_single() {
+
+	klon.querySelector("[data-restaurant_splash]").src = restaurant_single.acf.coverbillede.url;
+	klon.querySelector("[data-restaurant_name]").innerHTML = restaurant_single.title.rendered;
+	klon.querySelector("[data-restaurant-overskrift]").innerHTML = restaurant_single.title.rendered;
+	klon.querySelector("[data-restaurant-manchet]").innerHTML = restaurant_single.acf.manchet;
+	klon.querySelector("[data-restaurant-description]").innerHTML = restaurant_single.acf.beskrivelse;
+
+	klon.querySelector("[data-opening_hours_text]").innerHTML = "<h3>Åbningstider</h3>";
+	klon.querySelector("[data-contact_text]").innerHTML = "<h3>Kontakt</h3>";
+	klon.querySelector("[data-adress_text]").innerHTML = "<h3>Adresse</h3>";
+	klon.querySelector("[data-hygien_text]").innerHTML = "<h3>Kontrolrapport</h3>";
+
+	klon.querySelector("[data-opening-hours]").innerHTML = restaurant_single.acf.åbningstider;
+	klon.querySelector("[data-contact]").innerHTML = restaurant_single.acf.kontakt;
+	klon.querySelector("[data-adress]").innerHTML = restaurant_single.acf.adresse;
+	klon.querySelector("[data-hygien]").href = restaurant_single.acf.kontrolrapport;
+	document.querySelector("[data-page-title]").textContent = restaurant_single.title.rendered;
+
+	// console.log("Inserting image");
+
+	let galleri_modtager = document.querySelector("[data-gallery-modtager]");
+	let acf = restaurant_single.acf
+	let billeder = [
             acf.b_1,
             acf.b_2,
             acf.b_3,
@@ -60,91 +59,97 @@
             acf.b_5,
             acf.b_6
         ];
-        billeder.forEach(bild => {
-            let img = document.createElement("img");
-            img.src = bild
-    
-            galleri_modtager.appendChild(img)
-        })
+	billeder.forEach(bild => {
+		let img = document.createElement("img");
+		img.src = bild
 
-        modtager.appendChild(klon);
+		galleri_modtager.appendChild(img)
+	})
 
-
-        if( id === "190" ){
-            hentRetterJson();
-            document.querySelector('.menu_display').classList.remove('hidden');
-        }
+	modtager.appendChild(klon);
 
 
-    }
+	if (id === "190") {
+		hentRetterJson();
+		document.querySelector('.menu_display').classList.remove('hidden');
+	}
+	// ændre url efter / og erstatter med forhandlernavnet
+	let stateObj = {
+		forhandler_single: "navn"
+	};
 
-    let retter;
+	//history.pushState("object or string representing the state of the page", "new title", "newURL");
+	history.pushState(stateObj, "restaurant_single.title.rendered", restaurant_single.title.rendered);
 
-    let template_restaurant_menu = document.querySelector("[data-menu-restaurants]");
-    // console.log(template_restaurant_menu);
+}
 
-    async function hentRetterJson() {
+let retter;
 
-            let jsonObject = await fetch("http://josefinerasch.dk/kea/07-cms/food8/wordpress/wp-json/wp/v2/retter?per_page=100");
+let template_restaurant_menu = document.querySelector("[data-menu-restaurants]");
+// console.log(template_restaurant_menu);
 
-            retter = await jsonObject.json();
+async function hentRetterJson() {
 
-        visRetter();
+	let jsonObject = await fetch("http://josefinerasch.dk/kea/07-cms/food8/wordpress/wp-json/wp/v2/retter?per_page=100");
 
-    }
+	retter = await jsonObject.json();
 
-    let morgen_modtager = document.querySelector(".morgen_modtager");
-    let brunch_modtager = document.querySelector(".brunch_modtager");
-    let frokost_modtager = document.querySelector(".frokost_modtager");
-    let aften_modtager = document.querySelector(".aften_modtager");
-    let drikkevarer_modtager = document.querySelector(".drikkevarer_modtager");
-    let selskabsmenu_modtager = document.querySelector(".selskabsmenu_modtager");
+	visRetter();
 
+}
 
-    function visRetter(){
-
-			retter.forEach(ret => {
-
-
-                let menu_klon = template_restaurant_menu.cloneNode(true).content;
-//                menu_klon.querySelector("[data-dish-category]").innerHTML = ret.acf.kategori;
-//                menu_klon.querySelector("[data-dish-type]").innerHTML = ret.acf.type_af_ret;
-                menu_klon.querySelector("[data-price]").innerHTML = ret.acf.pris + " " +"kr";
-                menu_klon.querySelector("[data-dish-name]").innerHTML = ret.title.rendered;
-                menu_klon.querySelector("[data-description]").innerHTML = ret.acf.beskrivelse_af_ret;
+let morgen_modtager = document.querySelector(".morgen_modtager");
+let brunch_modtager = document.querySelector(".brunch_modtager");
+let frokost_modtager = document.querySelector(".frokost_modtager");
+let aften_modtager = document.querySelector(".aften_modtager");
+let drikkevarer_modtager = document.querySelector(".drikkevarer_modtager");
+let selskabsmenu_modtager = document.querySelector(".selskabsmenu_modtager");
 
 
-                ret.acf.kategori.forEach(kategoriString => {
-                    /**** Den enkelte rätts kategori ****/
-                    if ( kategoriString === "morgen"){
-                        morgen_modtager.appendChild(menu_klon);
-                    }
+function visRetter() {
 
-                    if ( kategoriString === "brunch"){
-                        brunch_modtager.appendChild(menu_klon);
-                    }
-                    if ( kategoriString === "frokost"){
-                        frokost_modtager.appendChild(menu_klon);
-                    }
-                    if ( kategoriString === "aften"){
-                        aften_modtager.appendChild(menu_klon);
-                    }
-                    if ( kategoriString === "drikkevarer"){
-                        drikkevarer_modtager.appendChild(menu_klon);
-                    }
-                    if ( kategoriString === "frokost"){
-                        selskabsmenu_modtager.appendChild(menu_klon);
-                    }
+	retter.forEach(ret => {
 
-                })
 
-                modtager.appendChild(menu_klon);
+		let menu_klon = template_restaurant_menu.cloneNode(true).content;
+		//                menu_klon.querySelector("[data-dish-category]").innerHTML = ret.acf.kategori;
+		//                menu_klon.querySelector("[data-dish-type]").innerHTML = ret.acf.type_af_ret;
+		menu_klon.querySelector("[data-price]").innerHTML = ret.acf.pris + " " + "kr";
+		menu_klon.querySelector("[data-dish-name]").innerHTML = ret.title.rendered;
+		menu_klon.querySelector("[data-description]").innerHTML = ret.acf.beskrivelse_af_ret;
 
-                
-            })
 
-            menuBtnClick();
-    }
+		ret.acf.kategori.forEach(kategoriString => {
+			/**** Den enkelte rätts kategori ****/
+			if (kategoriString === "morgen") {
+				morgen_modtager.appendChild(menu_klon);
+			}
+
+			if (kategoriString === "brunch") {
+				brunch_modtager.appendChild(menu_klon);
+			}
+			if (kategoriString === "frokost") {
+				frokost_modtager.appendChild(menu_klon);
+			}
+			if (kategoriString === "aften") {
+				aften_modtager.appendChild(menu_klon);
+			}
+			if (kategoriString === "drikkevarer") {
+				drikkevarer_modtager.appendChild(menu_klon);
+			}
+			if (kategoriString === "frokost") {
+				selskabsmenu_modtager.appendChild(menu_klon);
+			}
+
+		})
+
+		modtager.appendChild(menu_klon);
+
+
+	})
+
+	menuBtnClick();
+}
 
 
 
@@ -158,21 +163,20 @@ toggle between hiding and showing the dropdown content */
 function menuBtnClick() {
 
 
-    let modtagerSingle = document.querySelector('.modtager_single');
-    let menubtn = modtagerSingle.querySelector("[data-restaurant-menubtn]");
+	let modtagerSingle = document.querySelector('.modtager_single');
+	let menubtn = modtagerSingle.querySelector("[data-restaurant-menubtn]");
 
-    menubtn.addEventListener( 'click', (clickEvent) =>{
-        clickEvent.preventDefault()
-        document.querySelector("#myDropdown").classList.toggle("show");
-    })
-    
+	menubtn.addEventListener('click', (clickEvent) => {
+		clickEvent.preventDefault()
+		document.querySelector("#myDropdown").classList.toggle("show");
+	})
 
-    let menuSubBtns = modtagerSingle.querySelectorAll('#myDropdown > a') // Dette er nu et array.
 
-    menuSubBtns.forEach( submenuBtn => {
-        submenuBtn.addEventListener( 'click', clickEvent => {
-            document.querySelector("#myDropdown").classList.remove("show");
-        })
-    })
+	let menuSubBtns = modtagerSingle.querySelectorAll('#myDropdown > a') // Dette er nu et array.
+
+	menuSubBtns.forEach(submenuBtn => {
+		submenuBtn.addEventListener('click', clickEvent => {
+			document.querySelector("#myDropdown").classList.remove("show");
+		})
+	})
 }
-
