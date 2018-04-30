fc7239b79dfccb0c6ea804478b215f422280cf69
Bruger <bruger@laptop-8af4667f.kea.local>
2018-04-30 12:03:04 +0200

    book

--- header.js
+++ header.js
@@ -8,6 +8,11 @@ async function hentHeader() {
 
 	document.querySelector(".popup").style.pointerEvents = "none";
 	document.querySelector("[data-popup]").style.pointerEvents = "none";
+	document.querySelector("[data-kalenderImg]").style.pointerEvents = "none";
+	document.querySelector("[data-tidImg]").style.pointerEvents = "none";
+	document.querySelector("[data-ledigImg]").style.pointerEvents = "none";
+	document.querySelector("[data-ledigTid]").style.pointerEvents = "none";
+
 
 }
 
@@ -85,3 +90,44 @@ function hideNyhedsbrev2() {
 }
 
 ///////////////////////////book bord slut
+
+
+document.querySelector("[data-imgMockup]").addEventListener("click", showKalender);
+
+function showKalender() {
+	console.log("book");
+	document.querySelector("[data-kalenderImg]").style.pointerEvents = "auto";
+
+	document.querySelector("[data-kalenderImg]").classList.add("kalender");
+	document.querySelector("[data-kalenderImg]").style.opacity = "1";
+
+	document.querySelector("[data-kalenderImg]").addEventListener("click", showTid);
+}
+
+function showTid() {
+	console.log("tid");
+	document.querySelector("[data-tidImg]").style.pointerEvents = "auto";
+	document.querySelector("[data-ledigImg]").style.pointerEvents = "auto";
+
+	document.querySelector("[data-tidImg]").classList.add("tid");
+	document.querySelector("[data-tidImg]").style.opacity = "1";
+	document.querySelector("[data-ledigImg]").style.opacity = "1";
+	document.querySelector("[data-ledigImg]").classList.add("ledig");
+
+	document.querySelector("[data-tidImg]").addEventListener("click", showLedigTid);
+}
+
+function showLedigTid() {
+	console.log("ledig tid");
+	document.querySelector("[data-ledigTid]").style.pointerEvents = "auto";
+
+	document.querySelector("[data-ledigTid]").classList.add("ledig_tid");
+	document.querySelector("[data-ledigTid]").style.opacity = "1";
+
+	document.querySelector("[data-ledigTid]").addEventListener("click", hideKalenderBooking());
+}
+
+function hideKalenderBooking() {
+	console.log("hide kalender");
+
+}
